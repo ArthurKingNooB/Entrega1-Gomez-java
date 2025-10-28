8b2e4aef70374c7d1ca84cc9fe4771daae337fba
Arthur_Knigthon <150750143+ArthurKingNooB@users.noreply.github.com>
2025-10-28 20:37:35 -0300

    primeros cambios
    intente

---
+++ Entrega1Gomez/src/main/java/com/example/demo/Entrega1GomezApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entrega1GomezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Entrega1GomezApplication.class, args);
+	}
+
+}
